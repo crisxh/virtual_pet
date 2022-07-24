c096f1cc2da97f10a4db37bbd3cda36a3a8f09b5
crisxh <76125739+crisxh@users.noreply.github.com>
2022-07-24 06:48:24 +0200

    get and putaway pets methods

--- app.js
+++ app.js
@@ -3,18 +3,55 @@ $(function(){
     if(petTemp !=null){
         pets=petTemp;
     }
-console.log(pets);
-    // if(petTemp !=null){
-    //     for(let i=0;i<petTemp.length;i++){
-    //         pets.push(petTemp[i]);
-    
-    //     }
-       
-    //     console.log(pets);
-
-    // }
+    console.log(pets);
+    let petsKeys=Object.keys(pets);
+    let select=$('#selectPet select');
+    for (let i=0;i<petsKeys.length;i++){
+       console.log(petsKeys[i]);
+        let option=$(`<option></option`).html(petsKeys[i]);
+        select.append(option);
+    }
   
 })
+
+
+
+let view={
+    updateStatus:function(msg){
+        $('#statusBox').html(msg);
+        setTimeout(this.statusClear,1500);
+        },
+    appendPet(pet){
+        const petContainer=document.getElementById('petContainer');
+
+       let petDiv= $('<div>',{class:'pet',id:pet.name});
+       petDiv.css('background-color',pet.color).html(pet.name).addClass(pet.shape);
+       
+       if(!$.contains(document.getElementById('petContainer'), document.getElementById(pet.name))){
+        $('#petContainer').append(petDiv);
+        view.updateStatus(`${pet.name} says: "Hi!"`)
+       }
+       
+     
+    
+      
+
+    },
+    statusClear(){
+       $('#statusBox').html('');
+    },
+    putAwayPet(){
+        let pet=$('#selectPet select').val();
+        if($.contains(document.getElementById('petContainer'), document.getElementById(pet))){
+            $(`#${pet}`).remove();
+            view.updateStatus(`${pet} says: "bye bye"`)
+        
+           }
+           
+
+    }
+
+}
 
 function Pet(name,color,shape){
     this.shape=shape;
@@ -27,15 +64,26 @@ function Pet(name,color,shape){
 let pets={};
 
 function createPet(){
-    let pet=new Pet(prompt('name'),prompt('color'),prompt('shape'));
-   if (pet.name !=null && pet.shape !=null && pet.color !=null )
+    let petName=$('#name').val();
+    let petColor=$('#color').val();
+    let petShape=$("input[name='shape']:checked").val();
+    let pet=new Pet(petName,petColor,petShape);
+   if (pet.name !=null && pet.shape !=null && pet.color !=null && pet.name !="" )
     pets[pet.name]=pet;
     localStorage.setItem('pets',JSON.stringify(pets));
 }
 
+function toggleCreatePet(){
+    $('#createPetWindow').fadeOut();
+}
+
+
+
 function getPet(){
     let pet=$('#selectPet select').val();
-    console.log(pets[pet]);
+    console.log(pet);
+view.appendPet(pets[pet]);
+
 }
 
 $('#createPetBtn').click(()=>{
@@ -47,3 +95,16 @@ $('#getPet').click(()=>{
 getPet();
 })
 
+$('#close').click(()=>{
+    toggleCreatePet();
+})
+
+$('#putAwayPet').click(()=>{
+    view.putAwayPet();
+    console.log('put')
+    
+})
+
+$('#createPet').click(()=>{
+    $('#createPetWindow').fadeIn();
+})
